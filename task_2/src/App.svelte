c32a9f5b8578b7c01dfdf1999361d7267b72c87d
Xamst3r <47847269+Xamster3478@users.noreply.github.com>
2024-04-21 23:41:39 +0300

    Update App.svelte

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,6 +1,4 @@
 <script>
-  const apiKey = '167c4d4a5bf332d6538f2476';
-
   let amount1 = 1;
   let amount2 = 0;
   let rate = 1;
@@ -9,7 +7,7 @@
   let options = ['USD', 'EUR', 'GBP', 'RUB'];
 
   async function loadRate() {
-    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${selectedCurrency1}?apiKey=${apiKey}`);
+    const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${selectedCurrency1}`);
     const data = await response.json();
     rate = data.rates[selectedCurrency2];
     convert1to2();
@@ -23,29 +21,33 @@
     amount1 = (amount2 / rate).toFixed(2);
   }
 
-  $: if(selectedCurrency1 && selectedCurrency2) loadRate();
+  $: if (selectedCurrency1 && selectedCurrency2) loadRate();
 
-  $: if ($$self.$$.dirty & /*amount1*/ 1) {
-    convert1to2();
+  $: {
+    if (amount1) {
+      convert1to2();
+    }
   }
 
-  $: if ($$self.$$.dirty & /*amount2*/ 2) {
-    convert2to1();
+  $: {
+    if (amount2) {
+      convert2to1();
+    }
   }
 </script>
 
 <div>
-  <select bind:value={selectedCurrency1}>
+  <select bind:value="{selectedCurrency1}">
     {#each options as option}
-      <option>{option}</option>
+    <option>{option}</option>
     {/each}
   </select>
-  <input type="number" bind:value={amount1} on:input={convert1to2}/>
+  <input type="number" bind:value="{amount1}" on:input="{convert1to2}" />
 
-  <select bind:value={selectedCurrency2}>
+  <select bind:value="{selectedCurrency2}">
     {#each options as option}
-      <option>{option}</option>
+    <option>{option}</option>
     {/each}
   </select>
-  <input type="number" bind:value={amount2} on:input={convert2to1}/>
+  <input type="number" bind:value="{amount2}" on:input="{convert2to1}" />
 </div>
